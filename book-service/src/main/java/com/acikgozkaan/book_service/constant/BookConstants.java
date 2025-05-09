63e960cd917545e2b1eb8d1a620192848aba4ef0
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-05-10 01:33:48 +0300

    Add book service utils

---
+++ book-service/src/main/java/com/acikgozkaan/book_service/constant/BookConstants.java
@@ -0,0 +1,6 @@
+package com.acikgozkaan.book_service.constant;
+
+public class BookConstants {
+    public static final int MIN_PUBLICATION_YEAR = 1450;
+    public static final int MAX_PUBLICATION_YEAR = 2025;
+}
---
+++ book-service/src/main/java/com/acikgozkaan/book_service/mapper/BookMapper.java
@@ -0,0 +1,42 @@
+package com.acikgozkaan.book_service.mapper;
+
+import com.acikgozkaan.book_service.dto.BookRequest;
+import com.acikgozkaan.book_service.dto.BookResponse;
+import com.acikgozkaan.book_service.entity.Book;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookMapper {
+
+    public Book toBook(BookRequest request) {
+        return Book.builder()
+                .title(request.title())
+                .author(request.author())
+                .isbn(request.isbn())
+                .publicationYear(request.publicationYear())
+                .genre(request.genre())
+                .stock(request.stock())
+                .build();
+    }
+
+    public void updateBookFromRequest(Book book, BookRequest request) {
+        book.setTitle(request.title());
+        book.setAuthor(request.author());
+        book.setIsbn(request.isbn());
+        book.setPublicationYear(request.publicationYear());
+        book.setGenre(request.genre());
+        book.setStock(request.stock());
+    }
+
+    public BookResponse toResponse(Book book) {
+        return new BookResponse(
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getIsbn(),
+                book.getPublicationYear(),
+                book.getGenre(),
+                book.getStock()
+        );
+    }
+}
