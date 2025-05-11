219cbb82b8970f4db4bc07b96438f03f613926fd
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-05-11 17:01:54 +0300

    Add main class and pom.xml

--- borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/BorrowingServiceApplication.java
+++ borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/BorrowingServiceApplication.java
@@ -2,8 +2,10 @@ package com.acikgozkaan.borrowing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BorrowingServiceApplication {
 
 	public static void main(String[] args) {
