1506a55ad238169794f8fc6cad291db00de0ae69
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-05-11 16:56:04 +0300

    Add entity and repository

--- borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/entity/Borrowing.java
+++ borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/entity/Borrowing.java
@@ -1,4 +1,35 @@
 package com.acikgozkaan.borrowing_service.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "borrowings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Borrowing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private UUID bookId;
+
+    @Column(nullable = false)
+    private LocalDate borrowDate;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    private LocalDate returnDate;
 }
--- borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/repository/BorrowingRepository.java
+++ borrowing-service/src/main/java/com/acikgozkaan/borrowing_service/repository/BorrowingRepository.java
@@ -1,4 +1,14 @@
 package com.acikgozkaan.borrowing_service.repository;
 
-public class BorrowingRepository {
+import com.acikgozkaan.borrowing_service.entity.Borrowing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public interface BorrowingRepository extends JpaRepository<Borrowing, UUID> {
+
+    List<Borrowing> findByUserId(UUID userId);
+    List<Borrowing> findByDueDateBeforeAndReturnDateIsNull(LocalDate date);
 }
