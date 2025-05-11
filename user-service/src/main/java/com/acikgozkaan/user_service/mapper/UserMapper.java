2f7c6ba045a173fe6e4246daf32ebf2ecd696d08
Kaan Acikgoz <kaanacikgoz@outlook.com>
2025-05-12 02:44:58 +0300

    Add update request

--- user-service/src/main/java/com/acikgozkaan/user_service/mapper/UserMapper.java
+++ user-service/src/main/java/com/acikgozkaan/user_service/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.acikgozkaan.user_service.mapper;
 
 import com.acikgozkaan.user_service.dto.request.auth.RegisterRequest;
+import com.acikgozkaan.user_service.dto.request.user.UpdateUserRequest;
 import com.acikgozkaan.user_service.dto.response.user.UserResponse;
 import com.acikgozkaan.user_service.entity.Role;
 import com.acikgozkaan.user_service.entity.User;
@@ -36,4 +37,14 @@ public class UserMapper {
                 user.getAddress()
         );
     }
+
+    public void updateUserFromRequest(User user, UpdateUserRequest request) {
+        user.setEmail(request.email());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.setName(request.name());
+        user.setSurname(request.surname());
+        user.setPhone(request.phone());
+        user.setAddress(request.address());
+    }
+
 }
